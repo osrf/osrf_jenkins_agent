21173208ceb43e4c09d6bdb0fca3bc5d26b955df
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-11-11 16:02:03 +0100

    Implement new tests
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,11 +1,7 @@
 # InSpec test for recipe osrf_jenkins_agent::default
 
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe user(node['osrf_buildfarm']['agent']['agent_username']), :skip do
+  it { should exist }
 end
 
 # This is an example test, replace it with your own test.
@@ -13,3 +9,14 @@ describe port(80), :skip do
   it { should_not be_listening }
 end
 
+describe service("lightdm") do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+end
+
+describe service("jenkins-agent") do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+end
