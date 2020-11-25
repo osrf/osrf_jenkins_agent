565400f352ffe84aa1f0cbc0e47be069be3f9395
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-11-25 16:26:47 +0100

    Use just 1 executor

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['osrf_buildfarm']['agent']['java_args'] = ''
 default['osrf_buildfarm']['agent']['username'] = 'admin'
 default['osrf_buildfarm']['agent']['nodename'] = 'agent'
 default['osrf_buildfarm']['agent']['description'] = 'build agent'
-default['osrf_buildfarm']['agent']['executor'] = 4
+default['osrf_buildfarm']['agent']['executors'] = 1
 # TODO tags
 default['osrf_buildfarm']['agent']['labels'] = %w(docker)
 
