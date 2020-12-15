e5152223de433d58fada06afb2443a3c948c3a06
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-12-15 23:46:57 +0100

    Remove berks-vendor-cookbooks configuration

--- solo.rb
+++ solo.rb
@@ -1,4 +1,3 @@
 ROOT = File.expand_path("..", __FILE__)
 chef_repo_path ROOT
-cookbook_path [ File.join(ROOT, '..'), File.join(ROOT, 'berks-vendor-cookbooks')]
 environment_path File.join(ROOT, "test/integration/environments")
