c553a7af8f43ba125ca0f64a6300243037c9dcf2
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-01-22 18:56:29 +0100

    Include 'default' for default attribute values

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,9 @@
-default['osrf_buildfarm']['agent']['jenkins_url'] = "https://build.osrfoundation.org"
-default['osrf_buildfarm']['agent']['agent_username'] = 'jenkins'
+default['osrf_buildfarm']['agent']['jenkins_url'] = "https://default_url.org"
+default['osrf_buildfarm']['agent']['agent_username'] = 'default_agent_username'
 default['osrf_buildfarm']['agent']['java_args'] = ''
-default['osrf_buildfarm']['agent']['username'] = 'admin'
-default['osrf_buildfarm']['agent']['nodename'] = 'agent'
-default['osrf_buildfarm']['agent']['description'] = 'build agent'
+default['osrf_buildfarm']['agent']['username'] = 'default_username'
+default['osrf_buildfarm']['agent']['nodename'] = 'default_nodename'
+default['osrf_buildfarm']['agent']['description'] = 'default build agent description'
 default['osrf_buildfarm']['agent']['executors'] = 1
 # TODO tags
 default['osrf_buildfarm']['agent']['labels'] = %w(docker)
-
