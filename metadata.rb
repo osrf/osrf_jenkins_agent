c7fd840bcbac2c31f79de57a574bf761af8a2ae8
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-02-02 18:16:31 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,4 @@ long_description "Configures a Jenkins agent for the OSRF build farm."
 version "0.1.3"
 chef_version ">= 14.0"
 
-# "=4.6.7" # In sync with chef-osrf private configurations
 depends "docker"
