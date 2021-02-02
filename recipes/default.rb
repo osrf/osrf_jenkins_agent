28d6178e5461115a2d81a918b5d8c8fbd82229c9
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-02-02 17:23:27 +0100

    rubys method

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,24 +34,24 @@ end
 
 # GeForce GTX 550 Ti requires old 3xx.xx series
 package 'nvidia-384' do
-  only_if { has_nvidia_support() }
+  only_if { has_nvidia_support? }
 end
 
 cookbook_file '/etc/modprobe.d/blacklist-nvidia-nouveau.conf' do
   source 'blacklist-nvidia-nouveau.conf'
   mode '0744'
-  only_if { has_nvidia_support() }
+  only_if { has_nvidia_support? }
 end
 
 cookbook_file '/etc/X11/xorg.conf' do
   source 'xorg.conf.no_gpu'
   mode "0744"
-  not_if { has_nvidia_support() }
+  not_if { has_nvidia_support? }
 end
 cookbook_file '/etc/X11/xorg.conf' do
   source 'xorg.conf.nvidia'
   mode "0744"
-  only_if { has_nvidia_support() }
+  only_if { has_nvidia_support? }
 end
 # TODO: assuming :0 here is fragile
 ENV['DISPLAY'] = ':0'
@@ -149,7 +149,7 @@ ruby_block 'set node name' do
   block do
     node_name = "linux-#{node_base_name}.nv.focal"
   end
-  only_if { has_nvidia_support() }
+  only_if { has_nvidia_support? }
 end
 
 jenkins_username = node['osrfbuild']['agent']['username']
