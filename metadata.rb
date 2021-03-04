f2040b62f29d721d71e79779ff5c1632b2f07c59
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-03-05 00:49:00 +0100

    Implement full CI (#18)
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,6 @@ long_description "Configures a Jenkins agent for the OSRF build farm."
 version "0.1.3"
 chef_version ">= 14.0"
 
+# be careful if set a version on docker since it can conflict with other
+# repositories (chef-osrf)
 depends "docker"
