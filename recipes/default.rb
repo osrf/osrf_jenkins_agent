b7dec4ddc62c4375ce668ee07dd9eb9a06f23b93
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-03-16 15:52:35 +0100

    Fix repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -41,10 +41,10 @@ end
 for repo_uri in ['https://nvidia.github.io/libnvidia-container/stable/ubuntu18.04/$(ARCH)',
                 'https://nvidia.github.io/nvidia-container-runtime/stable/ubuntu18.04/$(ARCH)',
                 'https://nvidia.github.io/nvidia-docker/ubuntu18.04/$(ARCH)'] do
-    apt_repository "nvidia-docker#{repo_uri.hash}" do
+  apt_repository "nvidia-docker#{repo_uri.hash}" do
     uri repo_uri
+    distribution '/'
     key ['https://nvidia.github.io/nvidia-docker/gpgkey']
-    components ['main']
     action :add
     only_if { has_nvidia_support? }
   end
