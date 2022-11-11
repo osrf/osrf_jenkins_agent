c73aa086a63cfb2c823d3773fad73488658f6a88
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-11-11 18:43:16 +0100

    Refactor default to split build tools + agent connection
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,10 @@
 default['osrfbuild']['agent']['linux_username'] = 'default_linux_username'
 
+# If set to true, install common build tools for linux agents in the buildfarm
+# If set to false, install just the Jenkins agent connection. Useful for
+# special machines like the package repositories.
+default['osrfbuild']['agent']['install_agent_build_setup'] = false
+
 default['osrfbuild']['agent']['jenkins_url'] = "https://default_url.org"
 default['osrfbuild']['agent']['java_args'] = ''
 default['osrfbuild']['agent']['username'] = 'default_username'
