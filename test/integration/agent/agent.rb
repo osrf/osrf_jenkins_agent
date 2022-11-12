e12e422c52c5098587791ab295d5aa6b15c493f8
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-11-12 01:33:50 +0100

    Be sure that attributes are pure boolean
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- test/integration/agent/agent.rb
+++ test/integration/agent/agent.rb
@@ -26,3 +26,11 @@ control 'jenkins-agent' do
     it { should_not be_running }
   end
 end
+
+control 'no-x11-packages' do
+  impact 'low'
+  title 'Check that the system does not contain X11 packages'
+  describe service('lightdm') do
+    it { should_not be_installed }
+  end
+end
