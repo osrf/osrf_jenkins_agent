f8d3b9990977fdaa94abfc60b450f308a93442ce
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-03-08 18:26:51 -0800

    Try to resolve error on deploy.

--- recipes/macos.rb
+++ recipes/macos.rb
@@ -3,6 +3,7 @@ user 'jenkins' do
   comment 'jenkins'
   if node['osrf_jenkins_agent']['macos']['jenkins_user_password']
     password node['osrf_jenkins_agent']['macos']['jenkins_user_password']
+    secure_token_password node['osrf_jenkins_agent']['macos']['jenkins_user_password']
   end
 end
 
