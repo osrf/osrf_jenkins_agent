5086a09141ecfbdbc7ed5dcb8657a29b10c65975
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-03-08 18:37:58 -0800

    Give up on managing users and try setting a default.

--- recipes/macos.rb
+++ recipes/macos.rb
@@ -1,9 +1,5 @@
-user 'jenkins' do
-  home '/Users/jenkins'
-  comment 'jenkins'
-  if node['osrf_jenkins_agent']['macos']['jenkins_user_password']
-    password node['osrf_jenkins_agent']['macos']['jenkins_user_password']
-    secure_token_password node['osrf_jenkins_agent']['macos']['jenkins_user_password']
-  end
+macos_userdefaults "jenkins autologin" do
+  domain "/Library/Preferences/com.apple.loginwindow"
+  key "autoLoginUser"
+  value "jenkins"
 end
-
