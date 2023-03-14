b67f9dece92e772bc3c2877675c7664a49d4996e
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-03-14 10:28:03 -0700

    Only download and install the package if it is not already installed.

--- recipes/macos.rb
+++ recipes/macos.rb
@@ -2,10 +2,12 @@
 
 remote_file "/tmp/jdk8.pkg" do
   source "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u362-b09/OpenJDK8U-jdk_x64_mac_hotspot_8u362b09.pkg"
+  not_if "pkgutil --pkg-info net.temurin.8.jdk"
 end
 
 execute "install java" do
   command "installer -pkg /tmp/jdk8.pkg -target /"
+  not_if "pkgutil --pkg-info net.temurin.8.jdk"
 end
 
 # Create workspace
