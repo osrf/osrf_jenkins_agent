cfb3268202244d935a7859d11edca2296326a658
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-03-14 10:21:00 -0700

    Swtich gears to Jenkins setup.
    Base level macOS setup is too trial-and-error and will have to be done
    by hand for now.

--- recipes/macos.rb
+++ recipes/macos.rb
@@ -1,10 +1,17 @@
-macos_userdefaults "jenkins autologin" do
-  domain "/Library/Preferences/com.apple.loginwindow"
-  key "autoLoginUser"
-  user :all
-  value "jenkins"
+# Install java
+
+remote_file "/tmp/jdk8.pkg" do
+  source "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u362-b09/OpenJDK8U-jdk_x64_mac_hotspot_8u362b09.pkg"
 end
 
-execute "jenkins autologin" do
-  command "defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser jenkins"
+execute "install java" do
+  command "installer -pkg /tmp/jdk8.pkg -target /"
 end
+
+# Create workspace
+
+# Fetch swarm client jar
+
+# Create launch service
+
+# Start launch service
