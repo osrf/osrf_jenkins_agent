1a4aac5146ec4f607e50e0cfa913636a61dd05f4
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-03-15 16:13:27 -0700

    Add launchd service for running Jenkins.

--- recipes/macos.rb
+++ recipes/macos.rb
@@ -10,10 +10,49 @@ execute "install java" do
   not_if "pkgutil --pkg-info net.temurin.8.jdk"
 end
 
-# Create workspace
-
 # Fetch swarm client jar
+swarm_jar_path = "/Users/jenkins/swarm-client.jar"
+
+remote_file swarm_jar_path do
+  source "#{node['osrfbuild']['agent']['jenkins_url']}/swarm/swarm-client.jar"
+  owner "jenkins"
+  group "jenkins"
+end
+
+# Map macOS platform version to version identifier
+mac_version = case node["platform_version"] 
+              when/\A11\./
+                  "bigsur"
+              when /\A12\./
+                "monterey"
+              when /\A13\./
+                "ventura"
+              else
+                Chef::Fatal.log("macOS version #{node["platform_version"]} is not supported by this cookbook")
+                raise
+              end
+
+agent_name = "mac-#{node["hostname"]}.#{mac_version}"
+jenkins_agent_username = node['osrfbuild']['agent']['username']
+jenkins_agent_user = data_bag_item('osrfbuild_jenkins_users', jenkins_agent_username)
+labels = node['osrfbuild']['agent']['labels'] || Array.new
+if node['osrfbuild']['agent']['auto_generate_labels']
+  labels << "osx"
+  labels << "osx_#{mac_version}"
+end
 
 # Create launch service
+template "/Library/LaunchDaemons/org.osrfoundation.build.jenkins-agent.plist" do
+  source "jenkins-agent.plist.erb"
+  variables Hash[
+    swarm_jar_path: swarm_jar_path,
+    jenkins_url: node['osrfbuild']['agent']['jenkins_url'],
+    agent_name: agent_name,
+    username: jenkins_agent_user['username'],
+    token: jenkins_agent_user['password'],
+    description: "macOS #{mac_version} Jenkins agent",
+    labels: labels,
+  ]
+end
 
 # Start launch service
