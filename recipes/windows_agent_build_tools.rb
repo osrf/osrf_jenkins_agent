542f88295a16b8576632d0283e22160293980f5b
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-07 18:34:38 +0200

    Add more packages to provisioning
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- recipes/windows_agent_build_tools.rb
+++ recipes/windows_agent_build_tools.rb
@@ -2,9 +2,29 @@ chocolatey_package 'meinberg-ntp'
 # Used by release-tools particularly sdformat
 chocolatey_package 'wget'
 
-#  python 3.11.2 ruby 3.1.3 cmake 3.25 git 2.38.1 VS 2019 Community java 8 vcpkg-temp-cache
+#  VS 2019 Community java 8 vcpkg-temp-cache
+
+chocolatey_package 'python' do
+  version [ '3.11.2' ]
+end
+
+chocolatey_package 'ruby' do
+  version [ '3.1.3' ]
+end
+
 chocolatey_package 'cmake' do
   version [ '3.25' ]
+end
+
+chocolatey_package 'cuda' do
+end
+
+python_package 'vcstool' do
+  action :install
+end
+
+python_package 'colcon-common-extensions' do
+  action :install
 end
 
 # AdoptOpenJDK installer documentation https://adoptopenjdk.net/installation.html#windows-msi
@@ -14,3 +34,23 @@ windows_package 'openjdk' do
   action :install
 end
 
+windows_package 'Visual Studio 2019 Community' do
+  source 'https://aka.ms/vs/16/release/vs_community.exe'
+  installer_type :custom
+  options '--passive --installPath "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community"'
+  action :install
+end
+
+# TODO (j-rivero): grab the snapshot tab from release-tools once the
+# gz-collections.yaml is ready with that info
+git 'C:/vcpkg' do
+  repository 'https://github.com/microsoft/vcpkg.git'
+  revision '2022.02.23'
+  action :sync
+end
+
+execute 'bootstrap-vcpkg' do
+  command 'C:/vcpkg/bootstrap-vcpkg.bat'
+  cwd 'C:/vcpkg'
+  action :run
+end
