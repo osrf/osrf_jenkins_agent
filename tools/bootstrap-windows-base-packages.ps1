0b36cef62ca7cb7103703e0a02ec296a3f79608c
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-07 19:18:47 +0200

    Try to avoid berkshelf installation
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- tools/bootstrap-windows-base-packages.ps1
+++ tools/bootstrap-windows-base-packages.ps1
@@ -2,13 +2,10 @@ $ErrorActionPreference = 'Stop'
 . { iwr -useb https://omnitruck.cinc.sh/install.ps1 } | iex; install -version 17
 Set-ExecutionPolicy Bypass -Scope Process -Force;. { iwr -useb https://chocolatey.org/install.ps1 } | iex
 choco install -y git
-Install-Module -Name Berkshelf -Force -AllowClobber
 refreshenv
 
 Remove-Item -Path osrf_jenkins_agent -Force -Recurse -ErrorAction SilentlyContinue
 git clone --verbose --progress https://github.com/osrf/osrf_jenkins_agent.git -b jrivero/provision_packages
-cd osrf_jenkins_agent
-berks vendor berks-vendor-cookbooks
-cd tools
+cd osrf_jenkins_agent\tools
 C:\cinc-project\cinc\bin\cinc-solo.bat -c ..\solo.rb -j osrf-windows-agent-pkgs.solo.json 
 Write-Host "Completed chef-solo result $?"
