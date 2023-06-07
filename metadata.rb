3fd22a2e610b25693457e22af46230d0eca074a0
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-07 17:17:46 +0200

    Restrict docker to linux
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,8 @@ long_description "Configures a Jenkins agent for the OSRF build farm."
 version "0.1.4"
 chef_version ">= 14.0"
 
-# be careful if set a version on docker since it can conflict with other
-# repositories (chef-osrf)
-depends "docker"
+if platform?('linux')
+  # be careful if set a version on docker since it can conflict with other
+  # repositories (chef-osrf)
+  depends "docker"
+end
