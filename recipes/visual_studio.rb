1d8b505ed2634156e185baff9b550eb5cb42ee84
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-13 18:19:21 +0200

    Reuse visual studio from ros2
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ recipes/visual_studio.rb
@@ -0,0 +1,62 @@
+# For more information about command line arguments, see:
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
+
+# If BuildTools is not already installed, install it
+packages_to_install =  %w[
+  Microsoft.Component.MSBuild
+  Microsoft.Net.Component.4.6.1.TargetingPack
+  Microsoft.Net.Component.4.8.SDK
+  Microsoft.VisualStudio.Component.CoreBuildTools
+  Microsoft.VisualStudio.Component.Roslyn.Compiler
+  Microsoft.VisualStudio.Component.TextTemplating
+  Microsoft.VisualStudio.Component.VC.CLI.Support
+  Microsoft.VisualStudio.Component.VC.CoreBuildTools
+  Microsoft.VisualStudio.Component.VC.CoreIde
+  Microsoft.VisualStudio.Component.VC.Redist.14.Latest
+  Microsoft.VisualStudio.Component.VC.Tools.x86.x64
+  Microsoft.VisualStudio.Component.Windows10SDK
+  Microsoft.VisualStudio.Component.Windows10SDK.19041
+  Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core
+  Microsoft.VisualStudio.Workload.MSBuildTools
+  Microsoft.VisualStudio.Workload.VCTools
+]
+
+package_arguments = packages_to_install.map{ |pkg| '--add ' + pkg}.join(' ')
+base_options = '--quiet --wait --norestart '
+
+installer_options = base_options + package_arguments
+
+visual_studio_source = 'https://aka.ms/vs/16/release/vs_%s.exe' % node['ros2_windows']['vs_version']
+
+vs_version_camel_case = {
+  'buildtools' => 'BuildTools',
+  'community' => 'Community',
+  'professional' => 'Professional',
+  'enterprise' => 'Enterprise'
+}[node['ros2_windows']['vs_version']]
+
+visual_studio_path = 'c:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\%s' % vs_version_camel_case
+windows_package 'Update VS' do
+  source visual_studio_source
+  installer_type :custom
+  returns [0, 3010]
+  options 'update ' + installer_options
+  timeout 1200
+  only_if {::Dir.exist?(visual_studio_path)}
+end
+
+windows_package 'Install vs_buildtools' do
+  source visual_studio_source
+  installer_type :custom
+  action :install
+  returns [0, 1605, 1614, 1641, 3010]
+  options installer_options
+  timeout 1200
+  not_if {::Dir.exist?(visual_studio_path)}
+end
+
+windows_env 'VCTargetsPath' do
+  key_name 'VCTargetsPath'
+  value File.join(visual_studio_path, 'MSBuild\\Microsoft\\VC\\v160\\')
+  action :create
+end
--- recipes/windows_agent_build_tools.rb
+++ recipes/windows_agent_build_tools.rb
@@ -1,11 +1,10 @@
 include_recipe 'osrf_jenkins_agent::python'
 include_recipe 'osrf_jenkins_agent::pip_installs'
+include_recipe 'osrf_jenkins_agent::visual_studio'
 
 chocolatey_package 'meinberg-ntp'
-# Used by release-tools particularly sdformat
-chocolatey_package 'wget'
 
-#  VS 2019 Community java 8 vcpkg-temp-cache
+chocolatey_package 'wget'
 
 chocolatey_package 'ruby' do
   version [ '3.1.3.1' ]
@@ -17,18 +16,10 @@ end
 
 chocolatey_package 'cuda'
 
-
 # AdoptOpenJDK installer documentation https://adoptopenjdk.net/installation.html#windows-msi
 windows_package 'openjdk' do
   options 'INSTALLLEVEL=1 /quiet'
   source 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08_openj9-0.18.1/OpenJDK8U-jdk_x64_windows_openj9_8u242b08_openj9-0.18.1.msi'
-  action :install
-end
-
-windows_package 'Visual Studio 2019 Community' do
-  source 'https://aka.ms/vs/16/release/vs_community.exe'
-  installer_type :custom
-  options '--passive --installPath "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community"'
   action :install
 end
 
