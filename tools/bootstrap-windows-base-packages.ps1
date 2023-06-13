8628c069785b0d00db7e224236bd69e394de5f71
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-13 19:22:00 +0200

    Update bootstrap
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- tools/bootstrap-windows-base-packages.ps1
+++ tools/bootstrap-windows-base-packages.ps1
@@ -2,9 +2,10 @@ $ErrorActionPreference = 'Stop'
 Set-ExecutionPolicy Bypass -Scope Process -Force;. { iwr -useb https://chocolatey.org/install.ps1 } | iex
 choco install -y git chefdk
 refreshenv
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+Write-Host "NEEDS TO CLOSE POWERSHELL AND RUN AGAIN TO MAKE GIT WORK"
 
-Remove-Item -Path osrf_jenkins_agent -Force -Recurse -ErrorAction SilentlyContinue
 git clone --verbose --progress https://github.com/osrf/osrf_jenkins_agent.git -b jrivero/provision_packages
 cd osrf_jenkins_agent\tools
-C:\cinc-project\cinc\bin\cinc-solo.bat -c ..\solo.rb -j osrf-windows-agent-pkgs.solo.json 
+chef-solo.bat -c ..\solo.rb -j osrf-windows-agent-pkgs.solo.json
 Write-Host "Completed chef-solo result $?"
