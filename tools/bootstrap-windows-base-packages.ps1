53c39d5fa26b7924eb61a5e72bec1cfbe27248fb
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-06-20 17:23:41 +0200

    Update bootstrap-windows-base-packages.ps1

--- tools/bootstrap-windows-base-packages.ps1
+++ tools/bootstrap-windows-base-packages.ps1
@@ -5,7 +5,7 @@ refreshenv
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 Write-Host "NEEDS TO CLOSE POWERSHELL AND RUN AGAIN TO MAKE GIT WORK"
 
-git clone --verbose --progress https://github.com/osrf/osrf_jenkins_agent.git -b jrivero/provision_packages
+git clone --verbose --progress https://github.com/osrf/osrf_jenkins_agent.git
 cd osrf_jenkins_agent\tools
 chef-solo.bat -c ..\solo.rb -j osrf-windows-agent-pkgs.solo.json
 Write-Host "Completed chef-solo result $?"
